bb069189637be3363cf8e7bceaf9008144687855
Luca Di Bello <luca.dibello@samtrevano.ch>
2018-09-23 11:56:12 +0200

    Added usefull functions

--- I3Compiti/Helper/ClientHelper.cs
+++ I3Compiti/Helper/ClientHelper.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using I3Compiti.Model;
+using I3Compiti.Data;
+using System.Windows.Forms;
 
 namespace I3Compiti.Helper
 {
     public static class ClientHelper
     {
-        public static void StartCheck()
+        public static TimeSpan GetTimeDifference(DateTime startTime, DateTime endTime)
         {
-
+            return endTime - startTime;
         }
+
+        public static void NotifyEvents(GUI gui,Compito[] compiti)
+        {
+            foreach (Compito compito in compiti)
+            {
+                TimeSpan diff = GetTimeDifference(DateTime.Now,compito.Data);
+                Console.WriteLine(diff.TotalMinutes + " " + ApplicationSettings.NotifyEventMinutesBefore);
+
+                if (diff.TotalMinutes <= ApplicationSettings.NotifyEventMinutesBefore)
+                {
+                    GUIHelper.NotifyUser(gui, ApplicationSettings.NotifyTimeout, "Mancano " + diff.Minutes+" minuti!", compito.Materia + " - " + compito.Descrizione, ToolTipIcon.Warning);
+                }
+            }
+        }
+
     }
 }
